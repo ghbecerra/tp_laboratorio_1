54de962a90cc5f02296645482798ac87d9092df9
ghbecerra <ghbecerra@hotmail.com>
2018-04-13 23:35:31 -0300

    Add files via upload

---
+++ TP_1_Becerra/funciones.h
@@ -0,0 +1,53 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+/** \brief Realiza la suma de dos operandos
+ *
+ * \param Primer operando a sumar
+ * \param Segundo Operando a sumar
+ * \return Retorna la suma de los operandos
+ *
+ */
+
+float suma(float,float);
+
+/** \brief Realiza la resta de dos operandos
+ *
+ * \param Primer operando, minuendo
+ * \param Segundo Operando, sustraendo
+ * \return Retorna el resultado de la resta de los operandos
+ *
+ */
+
+float resta(float,float);
+
+/** \brief Realiza la division entre dos operandos
+ *
+ * \param Primer operando, dividendo
+ * \param Segundo operando, dividendo
+ * \return Retorna el resultado de la division, cociente
+ *
+ */
+
+float division(float,float);
+
+/** \brief Realiza el producto de dos factores
+ *
+ * \param Primer factor
+ * \param Segundo factor
+ * \return Retorna el producto de ambos factores
+ *
+ */
+
+float multiplicacion(float,float);
+
+/** \brief Realiza el factorial de un operando
+ *
+ * \param Operando
+ * \return Retorna el factorial del operando
+ *
+ */
+
+float factorial(float);
+
+#endif // FUNCIONES_H_INCLUDED
