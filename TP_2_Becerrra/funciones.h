12f1382cf2b0c7f9c9ae93d8cf9a9c2a28da4d85
ghbecerra <ghbecerra@hotmail.com>
2018-05-12 00:10:09 -0300

    Update funciones.h

--- TP_2_Becerrra/funciones.h
+++ TP_2_Becerrra/funciones.h
@@ -9,29 +9,61 @@ typedef struct
     int estado;
 }ePersona;
 
+/**
+ * Mostrar en pantalla menu de opciones.
+ * @param lista el array se pasa como parametro.
+ * @return opcion elegida por el usuario
+ */
 int imprimirMenu(void);
-void ePersona_init(ePersona listado[],int limite);
-void ePersona_alta(ePersona listado[],int limite);
-int ePersona_buscarLugarLibre(ePersona listado[],int limite);
-void ePersona_mostrarListado(ePersona listado[],int limite);
-void ePersona_ordenar(ePersona listado[],int limite);
-void ePersona_borrar(ePersona listado[],int limite);
-void ePersona_GraficoEdades(ePersona listado[],int limite);
 
+/**
+ * Inicializar estados.
+ * @param lista el array se pasa como parametro.
+ * @param capacidad maxima de array.
+ */
+void ePersona_init(ePersona listado[],int limite);
+
+/**
+ * Registrar datos en array.
+ * @param lista el array se pasa como parametro.
+ * @param capacidad maxima de array.
+ */
+void ePersona_alta(ePersona listado[],int limite);
 
 /**
  * Obtiene el primer indice libre del array.
  * @param lista el array se pasa como parametro.
+ * @param capacidad maxima de array.
  * @return el primer indice disponible
  */
-//int obtenerEspacioLibre(ePersona listado[]);
+int ePersona_buscarLugarLibre(ePersona listado[],int limite);
 
 /**
- * Obtiene el indice que coincide con el dni pasado por parametro.
+ * Imprimir en pantalla registro de individuos.
  * @param lista el array se pasa como parametro.
- * @param dni el dni a ser buscado en el array.
- * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
+ * @param capacidad maxima del array.
  */
-//int buscarPorDni(ePersona listado[], int dni);
+void ePersona_mostrarListado(ePersona listado[],int limite);
+
+/**
+ * Ordenar array de datos.
+ * @param lista el array se pasa como parametro.
+ * @param capacidad maxima del array.
+ */
+void ePersona_ordenar(ePersona listado[],int limite);
+
+/**
+ * Borrar individuo del array de datos.
+ * @param lista el array se pasa como parametro.
+ * @param capacidad maxima del array.
+ */
+void ePersona_borrar(ePersona listado[],int limite);
+
+/**
+ * Imprimir en pantalla grafico con edades de individuos cargados en array.
+ * @param lista el array se pasa como parametro.
+ * @param capacidad maxima del array.
+ */
+void ePersona_GraficoEdades(ePersona listado[],int limite);
 
 #endif // FUNCIONES_H_INCLUDED
